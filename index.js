c20de3c4c4bba37402de67c4428f998ad75ba2b4
Ohm-Rajpal <137588636+Ohm-Rajpal@users.noreply.github.com>
2024-07-05 19:00:39 -0700

    Adding express backend

---
+++ index.js
@@ -0,0 +1,32 @@
+// imports
+import express from "express"
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const app = express()
+const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url)) // get the os dir
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// create middleware
+function processMultipleFiles(req, res, next) {
+    // file processing here
+
+    next()
+}
+
+// run the custom middleware
+app.use(processMultipleFiles)
+
+// post request to handle the button click
+app.post('/extract', (req, res) => {
+    // res.sendFile() // send html file that shows a completion screen
+})
+
+// listen to the port
+app.listen(port, () => {
+    console.log(`Multiple file extractor listening on port ${port}`)
+})
